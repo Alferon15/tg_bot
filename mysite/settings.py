3685e87041c88caa88c750e105b38f7ce5aa3e47
alferon15 <alferon15@gmail.com>
2020-02-17 16:37:09 +1000

    v0.3

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o$u4#@x67cpybmo4+$n^@r^in$40-0n^q_q4yh6g++e-xteyjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://alferon15.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
