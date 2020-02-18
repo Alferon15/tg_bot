31f0f79a3b61e0da966b61367b7a5e1f75cc7799
alferon15 <alferon15@gmail.com>
2020-02-18 13:11:01 +1000

    v0.11 csrf

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -1,12 +1,13 @@
 import logging
 
 from telegram import Bot
-from telegram.ext import Updater
+# from telegram.ext import Updater
 
 from .bot_config import BOT_TOKEN, WEBHOOK_URL
 
 bot = Bot(BOT_TOKEN)
 
+bot.delete_webhook()
 bot.set_webhook(WEBHOOK_URL)
 
 logging.basicConfig(level=logging.INFO)
--- tgbot/views.py
+++ tgbot/views.py
@@ -1,15 +1,17 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
 from .services.tg_bot import *
 
 
+@csrf_exempt
 class TelegramBotView(View):
     def get(self, request):
-        updater.bot.send_message(100204219, 'GET')
+        bot.send_message(100204219, 'GET')
         return JsonResponse({"ok": "GET request processed"})
 
     def post(self, request):
-        updater.bot.send_message(100204219, 'POST')
+        bot.send_message(100204219, 'POST')
         return JsonResponse({"ok": "POST request processed"})
