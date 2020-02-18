eefac720c2a03c8f1352b04404050960e8dcf1dc
alferon15 <alferon15@gmail.com>
2020-02-18 11:57:29 +1000

    v0.5 python-telegram-bot

--- tgbot/services/bot_config.py
+++ tgbot/services/bot_config.py
@@ -4,4 +4,6 @@ BOT_TOKEN = '944887090:AAH8Udr9tTeILxWM33xGPkXeJcLmEpY_eio'
 WEBHOOK_HOST = 'https://alferon15.pythonanywhere.com/'
 WEBHOOK_PATH = SECRET
 
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
 admin_id = 100204219
--- tgbot/views.py
+++ tgbot/views.py
@@ -7,8 +7,9 @@ from .services.tg_bot import *
 
 class TelegramBotView(View):
     def get(self, request):
-        SendMessage(100204219, 'GET')
+        bot.send_message(100204219, 'GET')
         return JsonResponse({"ok": "GET request processed"})
 
     def post(self, request):
+        bot.send_message(100204219, 'POST')
         return JsonResponse({"ok": "POST request processed"})
