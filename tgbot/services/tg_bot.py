f68c02c4ea545ec4540370f607b17f95f3acb94b
alferon15 <alferon15@gmail.com>
2020-02-18 13:06:17 +1000

    v0.10 python-telegram-bot

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -3,19 +3,10 @@ import logging
 from telegram import Bot
 from telegram.ext import Updater
 
-from .bot_config import BOT_TOKEN, WEBHOOK_URL, SECRET
+from .bot_config import BOT_TOKEN, WEBHOOK_URL
 
 bot = Bot(BOT_TOKEN)
 
-updater = Updater(BOT_TOKEN)
-updater.start_webhook(
-    listen='0.0.0.0',
-    port=80,
-    url_path=SECRET,
-    key='certificate/private.key',
-    cert='certificate/cert.pem',
-    webhook_url=WEBHOOK_URL
-)
-updater.bot.set_webhook(WEBHOOK_URL, certificate='certificate/cert.pem')
+bot.set_webhook(WEBHOOK_URL)
 
 logging.basicConfig(level=logging.INFO)
