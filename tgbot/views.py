e3a2010a1ddc7b2e9e66e858fdd6ff08b29d5618
alferon15 <alferon15@gmail.com>
2020-02-18 16:14:22 +1000

    v0.2.4 Process messages

--- tgbot/views.py
+++ tgbot/views.py
@@ -7,11 +7,10 @@ from .services.tg_bot import *
 
 class TelegramBotView(View):
     def get(self, request):
-        bot.send_message(100204219, 'Главная страница посещена!')
-        bot.send_message(100204219, bot.get_webhook_info())
+        bot.send_message(100204219, 'Главная страница посещена!\n' + bot.get_webhook_info(timeout=1))
         return JsonResponse({"ok": "GET request processed"})
 
     def post(self, request):
-        msg = str(request.POST.data)
+        msg = str(request.POST)
         bot.send_message(100204219, msg)
         return JsonResponse({"ok": "POST request processed"})
