b7d4b7e625778e98b25fe6ea80b8a49d0df18e8d
alferon15 <alferon15@gmail.com>
2020-02-18 16:41:34 +1000

    v0.2.8 Process messages

--- tgbot/views.py
+++ tgbot/views.py
@@ -11,8 +11,6 @@ class TelegramBotView(View):
         return JsonResponse({"ok": "GET request processed"})
 
     def post(self, request):
-        msg = request
-        bot.send_message(100204219, msg.method)
-        bot.send_message(100204219, msg.body)
-        bot.send_message(100204219, msg.POST)
+        msg = request.read()
+        bot.send_message(100204219, msg)
         return JsonResponse({"ok": "POST request processed"})
