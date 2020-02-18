e36c6eef74b168efdf730b72cd0251dba166b2b3
alferon15 <alferon15@gmail.com>
2020-02-18 13:18:33 +1000

    v0.12 csrf

--- tgbot/urls.py
+++ tgbot/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from .views import TelegramBotView
 
 app_name = 'tg_bot'
 
 urlpatterns = [
-    path('', TelegramBotView.as_view(), name='index'),
+    path('', csrf_exempt(TelegramBotView.as_view()), name='index'),
 ]
