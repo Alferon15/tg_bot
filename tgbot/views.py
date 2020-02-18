ae2dfba925916539e56b6ebda48057ddefcb53d1
alferon15 <alferon15@gmail.com>
2020-02-18 13:23:19 +1000

    v0.13 csrf

--- tgbot/views.py
+++ tgbot/views.py
@@ -1,12 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
 from .services.tg_bot import *
 
 
-@csrf_exempt
 class TelegramBotView(View):
     def get(self, request):
         bot.send_message(100204219, 'GET')
