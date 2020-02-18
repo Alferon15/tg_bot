757e7d0cf6b17d2ab7f77a2dffa96b56b93708da
alferon15 <alferon15@gmail.com>
2020-02-18 12:31:47 +1000

    v0.7 python-telegram-bot

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -1,7 +1,7 @@
 import logging
 
 from telegram import Bot
-from telegram.ext import Dispatcher, Updater
+from telegram.ext import Updater
 
 from .bot_config import BOT_TOKEN, WEBHOOK_URL
 
@@ -11,4 +11,9 @@ updater = Updater(BOT_TOKEN)
 bot.delete_webhook()
 bot.set_webhook(WEBHOOK_URL)
 
+updater.start_webhook(
+    listen='0.0.0.0',
+    port=80,
+    webhook_url=WEBHOOK_URL
+)
 logging.basicConfig(level=logging.INFO)
