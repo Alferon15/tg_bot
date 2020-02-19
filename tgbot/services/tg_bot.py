c7888a02101e1053725d629adefd6fb9f289d05f
alferon15 <alferon15@gmail.com>
2020-02-19 15:19:23 +1000

    v0.3.3 Process messages

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -16,33 +16,14 @@ logging.basicConfig(level=logging.INFO)
 
 def process_updates(data):
     update = telebot.types.Update.de_json(data)
-    bot.process_new_updates([update])
+    # bot.process_new_updates([update])
     send_message_to_admin(update)
+    send_message_to_admin('---')
     return JsonResponse({"ok": True})
 
 
 def send_message_to_admin(msg):
     bot.send_message(admin_id, msg)
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    print('send_welcome')
-    bot.reply_to(message,
-                 ("Привет, я бот\n"
-                  "Старт!"))
-
-
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Привет, я бот\n"
-                  "Помощь!"))
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, f'Ты написал:\n{message.text}')
 
 
 bot.delete_webhook()
