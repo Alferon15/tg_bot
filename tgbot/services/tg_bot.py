d6036868b800eccc960e6c1849beeda5879c8e9a
alferon15 <alferon15@gmail.com>
2020-02-19 12:01:12 +1000

    v0.3.3 Process messages

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -9,15 +9,15 @@ telebot.logger.setLevel(logging.INFO)
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# bot.delete_webhook()
-# bot.set_webhook(WEBHOOK_URL)
+bot.delete_webhook()
+bot.set_webhook(WEBHOOK_URL)
 
 logging.basicConfig(level=logging.INFO)
 
 
 def process_updates(data):
     update = telebot.types.Update.de_json(data)
-    bot.process_new_updates(update)
+    bot.process_new_updates([update])
     # send_message_to_admin(f'---\n{update}\n---')
 
 
