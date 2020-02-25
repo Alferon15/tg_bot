b8baec72117f9121f7e448e6600d12030d154269
alferon15 <alferon15@gmail.com>
2020-02-25 11:12:51 +1000

    v0.3.3 Process messages

--- tgbot/models.py
+++ tgbot/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    class Meta:
+        verbose_name = 'Пользователь Телеграм'
+        verbose_name_plural = 'Пользователи Телеграм'
+
+    id = models.PositiveIntegerField(verbose_name='Telegram ID', primary_key=True)
+    username = models.CharField(verbose_name='Имя пользователя Telegram', max_length=50)
--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -5,6 +5,7 @@ import telebot
 
 from .bot_config import BOT_TOKEN, WEBHOOK_URL, admin_id
 
+from ..models import TelegramUser
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -17,10 +18,18 @@ logging.basicConfig(level=logging.INFO)
 def process_updates(data):
     update = telebot.types.Update.de_json(data)
     message = update.message
+    user_id = message.from_user.id
+    username = message.from_user.username
+
+    if not TelegramUser.objects.get(user_id):
+        TelegramUser.objects.create(user_id=user_id, username=username)
+
     msg = f'update_id - {update.update_id}\n' \
+          f'from_user - {message.from_user.id}\n' \
+          f'username - {message.from_user.username}\n\n' \
           f'content_type - {message.content_type}\n' \
-          f'from_user - {message.from_user.id}; username - {message.from_user.username}\n\n' \
           f'{message.text}'
+
     send_message_to_admin(msg)
     return JsonResponse({"ok": True})
 
