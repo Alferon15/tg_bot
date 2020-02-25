30f9172c313378bdf008407a232d54fd50dd4488
alferon15 <alferon15@gmail.com>
2020-02-25 12:15:47 +1000

    v0.3.5 Process messages

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -20,9 +20,7 @@ def process_updates(data):
     message = update.message
     user_id = message.from_user.id
     username = message.from_user.username
-
-    # if not TelegramUser.objects.get(int(user_id)):
-    #     TelegramUser.objects.create(user_id=user_id, username=username)
+    user = TelegramUser.objects.get_or_create(user_id=user_id, username=username)
 
     msg = f'update_id - {update.update_id}\n' \
           f'from_user - {message.from_user.id}\n' \
