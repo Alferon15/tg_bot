3a7dca203480c9a22f409e6ca07389cfb86d2f23
alferon15 <alferon15@gmail.com>
2020-02-25 12:43:14 +1000

    v0.3.5 Process messages

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -4,4 +4,4 @@ from .models import TelegramUser
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
-    fields = ['id', 'username', 'is_admin', 'is_allowed']
+    fields = ('id', 'username', 'is_admin', 'is_allowed')
