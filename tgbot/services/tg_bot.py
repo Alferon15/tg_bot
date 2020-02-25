e8b58dd24a3cc200a798b0a7824d98d5e3a4a928
alferon15 <alferon15@gmail.com>
2020-02-25 10:16:27 +1000

    v0.3.3 Process messages

--- tgbot/services/tg_bot.py
+++ tgbot/services/tg_bot.py
@@ -18,7 +18,7 @@ def process_updates(data):
     update = telebot.types.Update.de_json(data)
     send_message_to_admin(update)
     send_message_to_admin(update.message)
-    send_message_to_admin(update.message.chat)
+    send_message_to_admin(update.message.entities)
     return JsonResponse({"ok": True})
 
 
