647425cf3b744ae1b60fb60d3ad6cc0ec4174311
alferon15 <alferon15@gmail.com>
2020-05-25 21:49:34 +1000

    v0.2

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductIndexView
+from .views import ProductIndexView, ProductDetailView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductIndexView.as_view(), name='index'),
+    path('1', ProductDetailView.as_view(), name='detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class ProductIndexView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
     model = Product
